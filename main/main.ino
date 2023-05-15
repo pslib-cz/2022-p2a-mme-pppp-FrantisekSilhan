cb0bf3daa27f1fe1e15f324a19be7ef639646e92
František Šilhán <91247814+FrantisekSilhan@users.noreply.github.com>
2023-05-15 20:53:19 +0000

    ta krabička je odpad, ale ostatní se zdá býti done

--- main/main.ino
+++ main/main.ino
@@ -1,58 +1,61 @@
-//nastavení pinů tlačítek a deklarace globálních proměnných
-const int StepButtonPin = A5;
-const int GroupButtonPin = 4;
-const int ModeButtonPin = A4;
+#include <FastLED.h>
 
-bool lastStepButton = false;
+//piny
+#define MODE_LEDS_DATA_PIN 3
+#define GROUP_LEDS_DATA_PIN 2
+const int modeButtonPin = 5;
+const int groupButtonPin = 6;
+const int stepButtonPin = 4;
+
+//globální proměnné
+#define MODE_LEDS 1
+#define GROUP_LEDS 5
 bool lastModeButton = false;
 bool lastGroupButton = false;
+bool lastStepButton = false;
 
-
-int mode = 2;
-int groups = 0;
-int modeGroupGroup = 4;
-
-int modeYesNoChance = 50;
+const int modes = 3;
+int mode = -1;
+int group = -1;
 
 int modeRandomGroupData[5];
 int modeRandomGroupIndex = 0;
 
-void resetLEDs() {
-  // digitalWrite(A0, false);
-  // digitalWrite(A1, false);
-  // digitalWrite(A2, false);
-  digitalWrite(7, false);
-  digitalWrite(6, false);
-  digitalWrite(3, false);
-  digitalWrite(11, false);
-  digitalWrite(9, false);
+CRGB colors[5] = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::Cyan, CRGB::Yellow};
+
+CRGB mode_leds[MODE_LEDS];
+CRGB group_leds[GROUP_LEDS];
+
+int modeYesNoChance = 50;
+int modeGroupGroup = -1;
+bool yes;
+
+//funkce
+void setup() { 
+  FastLED.addLeds<WS2812B, MODE_LEDS_DATA_PIN, RGB>(mode_leds, MODE_LEDS);
+  FastLED.addLeds<WS2812B, GROUP_LEDS_DATA_PIN, RGB>(group_leds, GROUP_LEDS);
+  FastLED.setBrightness(50);
+
+  advanceMode();
 }
 
-//točení/výběr módu
+int modAdvance(int number, int mod) {
+  return (number + 1) % mod;
+}
+
 void advanceMode() {
-  resetLEDs();
-  mode = (mode + 1) % 3;
-
-  analogWrite(A0, 0 == mode ? 128 : 0);
-  analogWrite(A1, 1 == mode ? 128 : 0);
-  analogWrite(A2, 2 == mode ? 128 : 0);
+  mode = modAdvance(mode, modes);
+  mode_leds[0] = colors[mode];
 }
-//točení/výběr skupin
+
 void advanceGroup() {
-  resetLEDs();
-  groups += 1;
-  if (groups == 5) {
-    groups = 0;
+  group = modAdvance(group, GROUP_LEDS);
+  for (int i = 0; i < GROUP_LEDS; i++) {
+    group_leds[i] = i <= group ? colors[i] : CRGB::Black;
   }
-  digitalWrite(7, groups == 0);
-  digitalWrite(6, groups == 1);
-  digitalWrite(3, groups == 2);
-  digitalWrite(11, groups == 3);
-  digitalWrite(9, groups == 4);
 }
 
 
-//funkce s jednotlivými módy
 bool modeYesNo() {
   const int number = random(0, 100);
   if (number < modeYesNoChance) {
@@ -64,11 +67,22 @@ bool modeYesNo() {
   }
 }
 
-int modeRandomGroup() {
-  for (int i = 0; i <= groups; i++) {
-    int r = random(groups+1);
+
+void modeGroup(int number) {
+  if (number == -2) {
+    modeGroupGroup = modAdvance(modeGroupGroup, GROUP_LEDS);
+    number = modeGroupGroup;
+  }
+  for (int i = 0; i < GROUP_LEDS; i++) {
+    group_leds[i] = i == number ? colors[i] : CRGB::Black;
+  }
+}
+
+int generateRandomGroup() {
+  for (int i = 0; i <= group; i++) {
+    int r = random(group+1);
     bool found = false;
-    
+
     for (int j = 0; j < modeRandomGroupIndex; j++) {
       if (modeRandomGroupData[j] == r) {
         found = true;
@@ -78,7 +92,7 @@ int modeRandomGroup() {
     if (!found) {
       modeRandomGroupData[modeRandomGroupIndex] = r;
       modeRandomGroupIndex++;
-      if (modeRandomGroupIndex > groups) {
+      if (modeRandomGroupIndex > group) {
         modeRandomGroupIndex = 0;
         memset(modeRandomGroupData, 0, sizeof(modeRandomGroupData));
       }
@@ -87,99 +101,46 @@ int modeRandomGroup() {
   }
 }
 
-void switchLedFromNum(int num) {
-  switch (num) {
-    case 0:
-      digitalWrite(7, true);
-      break;
-    case 1:
-      digitalWrite(6, true);
-      break;
-    case 2:
-      digitalWrite(3, true);
-      break;
-    case 3:
-      digitalWrite(11, true);
-      break;
-    case 4:
-      digitalWrite(9, true);
-      break;
-  }
-}
-
-//funkce při stisknutí hlavního krokového tlačítka
 void step() {
-  resetLEDs();
   switch(mode) {
     case 0:
-      if (modeYesNo()) {
-        digitalWrite(9, true);
-      } else {
-        digitalWrite(7, true);
+      yes = modeYesNo();
+      for (int i = 0; i < GROUP_LEDS; i++) {
+          group_leds[i] = yes ? colors[0] : colors[1];
       }
       break;
     case 1:
-      modeGroupGroup = (modeGroupGroup + 1) % (groups + 1);
-      switchLedFromNum(modeGroupGroup);
+      modeGroup(-2);
       break;
     case 2:
-      switchLedFromNum(modeRandomGroup());
+      modeGroup(generateRandomGroup());
       break;
   }
-}
-
-void setup() {
-  Serial.begin(9600);
-  //nastavení ledek na zobrazení výstupu módů
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(9, OUTPUT);
-  
-  //nastavení ledek na zobrazení vybraného módu
-  pinMode(A0, OUTPUT);
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-
-  //nastavení vstupu tlačítek
-  pinMode(StepButtonPin, INPUT);
-  pinMode(GroupButtonPin, INPUT);
-  pinMode(ModeButtonPin, INPUT);
-
-  //nastavení prvního módu při startu
-  advanceMode();
-
-  //nastavení seedu z nezapojeného analogového pinu
-  randomSeed(analogRead(A3));
 }
 
 void loop() {
-  const bool stepButton = digitalRead(StepButtonPin);
-  const bool groupButton = digitalRead(GroupButtonPin);
-  const bool modeButton = digitalRead(ModeButtonPin);
-
-  if (lastStepButton != stepButton && stepButton) {
-    step();
-  }
-  if (lastGroupButton != groupButton && groupButton) {
-    advanceGroup();
-    modeGroupGroup = -1;
-
-    memset(modeRandomGroupData, 0, sizeof(modeRandomGroupData));
-    modeRandomGroupIndex = 0;
-  }
-  if (lastModeButton != modeButton && modeButton) {
+  const bool modeButton = digitalRead(modeButtonPin);
+  if (modeButton != lastModeButton && modeButton) {
     advanceMode();
+
     modeGroupGroup = -1;
-
-    memset(modeRandomGroupData, 0, sizeof(modeRandomGroupData));
-    modeRandomGroupIndex = 0;
   }
-
-  lastStepButton = stepButton;
-  lastGroupButton = groupButton;
   lastModeButton = modeButton;
 
-  delay(50);
+  const bool groupButton = digitalRead(groupButtonPin);
+  if (groupButton != lastGroupButton && groupButton) {
+    advanceGroup();
+
+    modeGroupGroup = -1;
+  }
+  lastGroupButton = groupButton;
+
+  const bool stepButton = digitalRead(stepButtonPin);
+  if (stepButton != lastStepButton && stepButton) {
+    step();
+  }
+  lastStepButton = stepButton;
+
+  delay(100);
+  FastLED.show();
 }
